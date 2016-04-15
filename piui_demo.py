45512ea0af9a3d783695d5a862e743ae9c0e1c5a
Michael Sanfilippo <sanfilippomike@gmail.com>
2016-04-15 00:24:05 -0700

    fixed indents
    fixed indents

--- piui_demo.py
+++ piui_demo.py
@@ -98,19 +98,19 @@ class DemoPiUi(object):
         con.print_line("Hello Console!")
 
     def page_video(self):
-    cap = cv2.VideoCapture(0)
-    while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Display the resulting frame
-        cv2.imshow('frame',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+        cap = cv2.VideoCapture(0)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Display the resulting frame
+            cv2.imshow('frame',gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                 break
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
 
     def main_menu(self):
         self.page = self.ui.new_ui_page(title="PiUi")
