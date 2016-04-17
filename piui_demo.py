0ef55fad2ba32c5355489788a2e00c2f53c7e94c
Michael Sanfilippo <sanfilippomike@gmail.com>
2016-04-17 00:55:37 -0400

    added detection
    added detection

--- piui_demo.py
+++ piui_demo.py
@@ -9,8 +9,8 @@ from picamera import PiCamera
 import time
 from piui import PiUi
 
-
-
+face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.1.0/data/haarcascades/haarcascade_eye.xml')
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -118,9 +118,17 @@ class DemoPiUi(object):
             image = frame.array
          
             # show the frame
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = image[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+     
             cv2.imshow("Frame", image)
-            #see if we can show on the UI? 
-            self.img = self.page.add_image(image)
             key = cv2.waitKey(1) & 0xFF
          
             # clear the stream in preparation for the next frame
